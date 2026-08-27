8cb56d0285157abc46f9058e8174a312fe1dc376
phai-nguyen <142614224+phai-nguyen@users.noreply.github.com>
2026-08-27 19:42:54 +0700

    Add files via upload

--- patches/emu_bridge.h
+++ patches/emu_bridge.h
@@ -157,7 +157,7 @@ namespace eka2l1::ios::bridge {
     std::vector<app_entry> get_all_apps();
     icon_image get_app_icon(std::uint32_t uid);
     void launch_app(std::uint32_t uid);
-    // Phone Mode V4: activate available Avkon/S60 UI support registrations before
+    // Phone Mode V5: activate available Avkon/S60 UI support registrations before
     // launching Home/Menu. Returns how many matching AppArc registrations were found.
     int prepare_system_ui_services();
 
